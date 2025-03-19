6ab240d532691d982dcd4b9a1ae9c99e7b9215b2
PiotrDratwa <piotr.k.dratwa@gmail.com>
2025-03-19 14:09:08 +0100

    added featured products page

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import {Button} from '@/components/ui/button'
-import { Ghost } from 'lucide-react';
 import Link from 'next/link'
+import FeaturedProducts from '@/components/home/FeaturedProducts'
+import Hero from '@/components/home/Hero';
 
 function HomePage() {
   return <div>
-    <h1 className='text-3xl'>HomePage</h1>
-    <Button variant='default' size='lg' asChild>
-      <Link href='/about'>About</Link>
-    </Button>
+      <Hero />
+      <FeaturedProducts />
     </div>;
 }
 export default HomePage;
---
+++ utils/actions.ts
@@ -0,0 +1,16 @@
+import db from '@/utils/db'
+
+export const fetchFeaturedProducts = async ()=>{
+    const products = db.product.findMany(
+        {where:
+            {featured:true}
+        }
+    )
+    return products
+}
+
+export const fetchAllProducts = ()=>{
+    return db.product.findMany({
+        orderBy:{createdAt:'desc'}
+    })
+}
